fe5c6015753a60d62ff15bbcf960e8322b8aee76
myllenaalves <myllenaalves971@gmail.com>
2021-05-02 21:23:40 -0300

    Implementando página inicial

--- app/src/main/java/com/example/resgatemeupet/MainActivity.kt
+++ app/src/main/java/com/example/resgatemeupet/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.resgatemeupet
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
 
 class MainActivity : AppCompatActivity() {
+    lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun toLogin(view: View?) {
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun toCadastro(view: View?) {
+        val intent = Intent(this, SignUpActivity::class.java)
+        startActivity(intent)
     }
 }
